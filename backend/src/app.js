adaee17a8150f6029d0c0b15376593cc4f138280
Dev-Alexis-Laris <diegolaris72@gmail.com>
2026-06-26 09:29:14 -0600

    fixes

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,11 +14,12 @@ import savings from "./routes/savings.routes.js";
 dotenv.config();
 
 const app = express();
+
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
 }));
 
 app.use(express.json());
